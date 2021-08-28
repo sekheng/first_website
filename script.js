eeb65d26841725cfe5305bd5795cd706aef93e35
sekheng <leesekheng@yahoo.com>
2021-08-28 12:38:24 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello!')
+var profilepic = document.getElementById('profilepic');
+profilepicSource = profilepic.src;
+var changeFlag = true;
+alternateProfilePic = 'https://imgflip.com/s/meme/Heavy-Breathing-Cat.jpg';
+
+function MoveImg() {
+  console.log(typeof profilepic);
+  //alert(image.style.left);
+  if (changeFlag) {
+    profilepic.src = profilepicSource;
+  } else {
+    profilepic.src = alternateProfilePic;
+  }
+  changeFlag = !changeFlag;
+}
+
+function sourceProfilePic() {
+  
+}
